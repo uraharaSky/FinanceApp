50661bc6dca05cb4478679e081a342ffadd599ef
Akash Biswal <akashbiswaliit7@gmail.com>
2025-05-01 12:23:53 +0530

    Drawables added

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -38,6 +38,7 @@ android {
     }
     buildFeatures {
         compose = true
+        viewBinding = true
     }
     composeOptions {
         kotlinCompilerExtensionVersion = "1.5.1"
@@ -66,4 +67,7 @@ dependencies {
     androidTestImplementation(libs.androidx.ui.test.junit4)
     debugImplementation(libs.androidx.ui.tooling)
     debugImplementation(libs.androidx.ui.test.manifest)
+    implementation("com.github.Philjay:MPAndroidChart:v3.1.0")
+    implementation("com.github.bumptech.glide:glide:4:13.0")
+
 }
